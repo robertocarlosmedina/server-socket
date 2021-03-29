450a2a0a72307d3921c07cd81092d1f066f10f09
Roberto-04-dev <true4321king@gmail.com>
2021-03-29 22:12:11 -0100

    Addinf start interface

--- components/start.py
+++ components/start.py
@@ -1,7 +1,8 @@
-from typing import Text
 import pygame
+from pygame.locals import *
 from components.colors import color, rgbColor
 from components.textInput import Textinput
+from components.buttons import verticalButtonsDisplay
 
 
 class Start:
@@ -19,18 +20,22 @@ class Start:
         self.timer = 0
         self.user = " "
         self.created = False
+        self.buttons = ["Log"]
 
-    def run(self, all_event, screen, screen_size):
+    def run(self, all_event, screen, screen_size, user):
+        del user
+        mouse_pos = pygame.mouse.get_pos()
         for event in all_event:
             if event.type == pygame.KEYDOWN:
                 if pygame.key.get_pressed()[K_RETURN]:
                     self.created = True
         size = pygame.font.Font.size(self.font, 'AlgorDataStruct')
         line = self.font.render('AlgorDataStruct', True, color.white.value)
-        screen.blit(line, (screen_size[0]/2-size[0]/2, screen_size[1]/2-size[1]/2))
+        screen.blit(line, (int(screen_size[0]/2-size[0]/2), int(screen_size[1]/2-size[1]/2)-60))
         self.user = self.text.settingInputText(all_event, screen)
+        
+        verticalButtonsDisplay(screen, self.buttons, 205, (int(screen_size[0]/2), int(screen_size[1]/2)), (150, 40),mouse_pos, " ", self.font)
         if self.created:
-
             return "chatbox", self.user
         else:
             return "start", self.user
